c771d92f783fc3ccf16ab262787f8eaf5b18ea3a
Sergio Rodríguez <84103237+ssergiorrodriguez@users.noreply.github.com>
2023-03-04 13:24:46 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let input1,input2,input3,button;
 let red1,green1,blue1;
 
 function setup() {
-  createCanvas(fullScreen);
+  createCanvas(Fullscreen);
   background(0);
   textSize(75);
   text("PERMUTATION-12", 220,80);
